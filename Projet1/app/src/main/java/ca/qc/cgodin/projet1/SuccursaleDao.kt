f0321e1a71d0f03b95799286b4de58bbb0d38bc8
Ghilyane <97771627+Ghilyane@users.noreply.github.com>
2023-10-05 16:32:19 -0400

    commit3

--- Projet1/app/src/main/java/ca/qc/cgodin/projet1/SuccursaleDao.kt
+++ Projet1/app/src/main/java/ca/qc/cgodin/projet1/SuccursaleDao.kt
@@ -6,7 +6,21 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
+import ca.qc.cgodin.projet1.model.Succursale
 
 @Dao
 interface SuccursaleDao {
+    @Query("SELECT * from succursale_table ORDER BY Ville ASC")
+    fun getSuccursales(): LiveData<List<Succursale>>
+    @Query("SELECT * FROM succursale_table WHERE Aut=(:Aut)")
+    fun getSuccursale(url: String): LiveData<Succursale?>
+    @Delete
+    fun delete(succursale: Succursale)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(succursale: Succursale)
+    @Update
+    fun updateSuccursale(succursale: Succursale)
+    @Query("DELETE FROM succursale_table")
+    fun deleteAll()
 }
---
+++ Projet1/app/src/main/java/ca/qc/cgodin/projet1/SuccursaleViewModel.kt
@@ -0,0 +1,4 @@
+package ca.qc.cgodin.projet1
+
+class SuccursaleViewModel {
+}
