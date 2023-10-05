adc6d3918a02744a6a8ee6cb56eede0ca0d865dd
Ghilyane <97771627+Ghilyane@users.noreply.github.com>
2023-10-05 16:23:47 -0400

    commit2

---
+++ Projet1/app/src/main/java/ca/qc/cgodin/projet1/SuccursaleDao.kt
@@ -0,0 +1,12 @@
+package ca.qc.cgodin.projet1
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface SuccursaleDao {
+}
