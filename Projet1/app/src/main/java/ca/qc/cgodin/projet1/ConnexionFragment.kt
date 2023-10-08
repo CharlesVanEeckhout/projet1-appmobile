e7b7080d05e4fa705570d9573436b5f0e29fd4d4
CharlesVanEeckhout <vaneeckhoutcharles@gmail.com>
2023-10-08 16:05:44 -0400

    lister succursales du server

--- Projet1/app/src/main/java/ca/qc/cgodin/projet1/ConnexionFragment.kt
+++ Projet1/app/src/main/java/ca/qc/cgodin/projet1/ConnexionFragment.kt
@@ -5,11 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import ca.qc.cgodin.projet1.databinding.FragmentConnexionBinding
+import ca.qc.cgodin.projet1.databinding.FragmentListSuccursalesBinding
+import ca.qc.cgodin.projet1.model.schema.Login
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -17,16 +16,13 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ConnexionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentConnexionBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var viewModel: SuccursaleViewModel // TODO: Jai oublie comment apporter le viewmodel dans le fragment, ca devrait etre ds labo 8
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -34,7 +30,13 @@ class ConnexionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_connexion, container, false)
+        _binding = FragmentConnexionBinding.inflate(layoutInflater, container, false)
+
+        binding.btnConnexion.setOnClickListener {
+            //val login = Login(binding.editID.text, binding.editMDP.text)
+            //viewModel.connexionStudent(...)
+        }
+        return binding.root
     }
 
     companion object {
@@ -42,18 +44,9 @@ class ConnexionFragment : Fragment() {
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
          * @return A new instance of fragment ConnexionFragment.
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ConnexionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance(param1: String, param2: String) = ConnexionFragment()
     }
 }
--- Projet1/app/src/main/java/ca/qc/cgodin/projet1/ListSuccursalesFragment.kt
+++ Projet1/app/src/main/java/ca/qc/cgodin/projet1/ListSuccursalesFragment.kt
@@ -6,8 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import ca.qc.cgodin.projet1.databinding.FragmentEditSuccursaleBinding
 import ca.qc.cgodin.projet1.databinding.FragmentListSuccursalesBinding
+import ca.qc.cgodin.projet1.model.response.ListeSuccursaleResponse
+import ca.qc.cgodin.projet1.model.schema.ListeSuccursaleSchema
+import com.google.gson.Gson
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Response
 
 private const val ARG_AUT = "Aut"
 
@@ -17,17 +22,21 @@ private const val ARG_AUT = "Aut"
  * create an instance of this fragment.
  */
 class ListSuccursalesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var aut: Long? = null
+    private var _aut: Long? = null
+    private val aut get() = _aut!!
 
     private var _binding: FragmentListSuccursalesBinding? = null
     private val binding get() = _binding!!
     private lateinit var application : Application;
+    private lateinit var viewModel: SuccursaleViewModel // TODO: jai oublie comment apporter le viewmodel dans le fragment, ca devrait etre ds labo 8
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            aut = it.getLong(ARG_AUT)
+            _aut = it.getLong(ARG_AUT,-1)
+        }
+        if(aut < 0 || aut > 1e12-1){
+            throw Exception("aut invalide: " + aut.toString())
         }
     }
 
@@ -45,6 +54,20 @@ class ListSuccursalesFragment : Fragment() {
 
         }
 
+        val succursaleListAdapter = SuccursaleListAdapter(inflater.context)
+        binding.recyclerView.adapter = succursaleListAdapter
+
+        viewModel.listeSuccursale(ListeSuccursaleSchema(aut),
+            { _: Call<ResponseBody>, response: Response<ResponseBody> ->
+                val responseJson: String = response.body()?.string() ?: "null"
+                val listeSuccursale: ListeSuccursaleResponse = Gson().fromJson(responseJson, ListeSuccursaleResponse::class.java)
+                succursaleListAdapter.setSuccursales(listeSuccursale.succursales)
+            },
+            { _: Call<ResponseBody>, t: Throwable ->
+                // TODO: "Echec du chargement de vos succursales. Reessayez plus tard."
+            })
+
+        // TODO: setonclick boutons edit et delete des item du adapter
 
 
         // Inflate the layout for this fragment
@@ -65,7 +88,6 @@ class ListSuccursalesFragment : Fragment() {
          * @param aut Parameter 1.
          * @return A new instance of fragment ListSuccursalesFragment.
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(aut: Long) =
             ListSuccursalesFragment().apply {
